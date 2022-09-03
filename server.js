eb0ab7b2b3b1209b15815763b4241ffaba2bad37
semetea <55260856+semetea@users.noreply.github.com>
2022-09-03 14:40:47 -0400

    query resolver

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  {
+    id: "1",
+    text: "first one!",
+  },
+  {
+    id: "2",
+    text: "second one!",
+  },
+];
+
 const typeDefs = gql`
   type User {
     """
@@ -13,15 +24,15 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
 
   type Query {
     """
     allTweets: []! -> this function return list, allTweets: [Tweet!]! -> this function return list filled with Tweet
     """
-    allTweeets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
   }
 
   type Mutation {
@@ -30,7 +41,18 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
