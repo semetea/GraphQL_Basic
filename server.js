6da7f11fe56f7d0e1ba38cad50df01d36fc8a1cf
semetea <55260856+semetea@users.noreply.github.com>
2022-09-03 14:51:10 -0400

    mutation resolver

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text: "first one!",
@@ -48,6 +48,24 @@ const resolvers = {
     },
     tweet(root, { id }) {
       return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      // 첫번째는 root, 두번째는 args
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
     },
   },
 };
