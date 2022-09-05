c891c0398195ef7b16b3da0292d3a6d69731c8c1
unknown <taewooocean@gmail.com>
2022-09-05 16:16:19 -0300

    mirgrating from rest to graphql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
   {
@@ -35,8 +36,14 @@ const typeDefs = gql`
     username: String!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of firstName + lastName
+    """
     fullName: String!
   }
+  """
+  Tweet object represents a resource for a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
@@ -47,14 +54,43 @@ const typeDefs = gql`
     """
     allTweets: []! -> this function return list, allTweets: [Tweet!]! -> this function return list filled with Tweet
     """
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet!
+    movie(id: String!): Movie!
   }
 
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
+  }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -69,6 +105,16 @@ const resolvers = {
     allUsers() {
       console.log("all users called!");
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((response) => response.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, id) {
+      return fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)
+        .then((response) => response.json())
+        .then((json) => json.data.movies);
     },
   },
 
